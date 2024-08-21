57f5631c7d86412ae0f53909b8c54fdb1e3cf03c
Aftab3008 <shaikaftab005@mail.com>
2024-08-21 14:31:35 +0530

    Minor changes 2

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,5 +1,5 @@
-import { db } from "@/lib/db";
-import { stripe } from "@/lib/stripe";
+import { db } from "../../../lib/db";
+import { stripe } from "../../../lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
