e95502a1664b88ee0b3ab58b2191d438cc945916
Aftab3008 <shaikaftab005@mail.com>
2024-08-21 21:08:21 +0530

    Error fix 4

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:auth*",
-        destination: "https://casecobra3008.kinde.com/:auth*",
+        destination: "https://casecobra3008.kinde.com/*",
       },
     ];
   },
