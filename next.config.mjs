14f2e3ada56a0aa86b0700a17d7769b6b39b3f2d
Aftab3008 <shaikaftab005@mail.com>
2024-08-21 20:57:08 +0530

    Error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://casecobra-drab-psi.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,HEAD,PUT,PATCH,POST,DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:auth*",
+        destination: "https://casecobra-drab-psi.vercel.app/",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
