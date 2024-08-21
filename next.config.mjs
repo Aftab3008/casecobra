19431f49c0086e524c0ee4a19bd2a99059a230d9
Aftab3008 <shaikaftab005@mail.com>
2024-08-22 00:56:56 +0530

    Error fix 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://casecobra-drab-psi.vercel.app/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
